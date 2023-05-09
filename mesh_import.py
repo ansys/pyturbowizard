0851af27aa87659199b589dd48c42ebd9e25f3dc
hauke2007 <108066434+hauke2007@users.noreply.github.com>
2023-05-09 10:52:28 +0200

    Update and rename mesh_import.py to meshimport.py

--- mesh_import.py
+++
@@ -1,7 +0,0 @@
-
-def import_01(caseEl):
-    if ".def" in caseEl["meshFilename"]:
-        solver.file.import_.read(file_type="cfx-definition", file_name=caseEl["meshFilename"])
-    elif ".msh" in caseEl["meshFilename"]:
-        print("to do")
-    return True
