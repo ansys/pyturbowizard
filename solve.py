8b69120c513fea7aa62c697f605080baa1f921af
hauke2007 <hauke.reese@ansys.com>
2023-05-09 11:07:00 +0200

    v2

--- solve.py
+++ solve.py
@@ -1,4 +1,10 @@
-def solve_01():
+def init_01(data, solver):
+    solver.tui.solve.initialize.compute_defaults.pressure_inlet(bz_inlet_name)
+    solver.solution.initialization.standard_initialize()
+    solver.solution.initialization.fmg_initialize()
+
+def solve_01(data, solver):
+    solver.solution.run_calculation.iterate(iter_count=iter_count)
     return
 
 
