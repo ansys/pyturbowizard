d39efc63bb4a18765e97d1e935c0cb7f3eb8b6ce
hauke2007 <108066434+hauke2007@users.noreply.github.com>
2023-05-09 10:53:15 +0200

    Update numerics.py

--- numerics.py
+++ numerics.py
@@ -1,2 +1,6 @@
-def strategy_01():
+def numerics_01(data, solver):
+    solver.solution.methods.gradient_scheme = "green-gauss-node-based"
+    if data["solution"]["tsn"]:
+        solver.tui.solve.set.advanced.turbomachinery_specific_numerics.enable('yes')
     return
+
