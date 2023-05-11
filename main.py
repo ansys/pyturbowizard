d4881cd2991b8df170b3adc593397affa87ad1bb
gantnera <128615962+gantnera@users.noreply.github.com>
2023-05-11 09:23:04 +0200

    Agantner develop (#3)
    argument can be used to specify the full path to config file

--- main.py
+++ main.py
@@ -2,23 +2,28 @@
 
 import os
 import json
+import sys
 
-#test push
-json_file = open('turboSetupConfig.json')
+json_filename = sys.argv[1]
+if json_filename is None:
+    json_filename = 'turboSetupConfig.json'
+
+json_file = open(json_filename)
 turboData = json.load(json_file)
 
-myLaunchEl = turboData.get("launching")
-external = myLaunchEl.get("external")
+launchEl = turboData.get("launching")
+external = launchEl.get("external")
 
 if external:
     import ansys.fluent.core as pyfluent
-    #import utilities import writeExpressionFile
+
     import utilities
     import meshimport
     import mysetup
     import numerics
     import postproc
     import solve
+    import parametricstudy
 
 else:
     from importlib.machinery import SourceFileLoader
@@ -28,6 +33,7 @@ else:
     numerics = SourceFileLoader("numerics", "./numerics.py").load_module()
     postproc = SourceFileLoader("postproc", "./postproc.py").load_module()
     solve = SourceFileLoader("solve", "./solve.py").load_module()
+    parametricstudy = SourceFileLoader("parametricstudy", "./parametricstudy.py").load_module()
 
 
 #pyfluent.set_log_level('DEBUG')
@@ -49,8 +55,8 @@ working_Dir = os.path.normpath(turboData["launching"]["workingDir"])
 
 if external:    # Fluent without pyConsole
     global solver
-    serverfilename = myLaunchEl.get("serverfilename")
-    if serverfilename is None:
+    serverfilename = launchEl.get("serverfilename")
+    if serverfilename is None or serverfilename == "":
         solver = pyfluent.launch_fluent(precision=turboData["launching"]["precision"], processor_count=int(turboData["launching"]["noCore"]),
                                     mode="solver", show_gui=True,
                                    product_version = turboData["launching"]["fl_version"], cwd=working_Dir)
@@ -61,60 +67,73 @@ if external:    # Fluent without pyConsole
 
 
 # Start Setup
-for casename in turboData["cases"]:
-    print("Running Case: " + casename + "\n")
-    caseEl = turboData["cases"][casename]
-    trnFileName = casename + ".trn"
+caseDict = turboData.get("cases")
+if not (caseDict is None):
+    for casename in caseDict:
+        print("Running Case: " + casename + "\n")
+        caseEl = turboData["cases"][casename]
+        trnFileName = casename + ".trn"
 
-    solver.file.start_transcript(file_name=trnFileName)
-    #
+        solver.file.start_transcript(file_name=trnFileName)
+        #
 
-    # Mesh import, expressions, profiles
-    result = meshimport.import_01(caseEl, solver)
+        # Mesh import, expressions, profiles
+        result = meshimport.import_01(caseEl, solver)
 
-    utilities.writeExpressionFile(caseEl,working_Dir)
-    solver.tui.define.named_expressions.import_from_tsv(caseEl["expressionFilename"])
+        utilities.writeExpressionFile(caseEl,working_Dir)
+        solver.tui.define.named_expressions.import_from_tsv(caseEl["expressionFilename"])
 
-    # Enable Beta-Features
-    solver.tui.define.beta_feature_access("yes ok")
-
-
-    # Case Setup
-    mysetup.setup_01(caseEl, solver)
-
-    mysetup.report_01(caseEl, solver)
-    #Solution
+        # Enable Beta-Features
+        solver.tui.define.beta_feature_access("yes ok")
 
 
-       #Set Solver Settings
-    numerics.numerics_01(caseEl, solver)
-    #Activate Turbonumerics
+        # Case Setup
+        mysetup.setup_01(caseEl, solver)
+
+        mysetup.report_01(caseEl, solver)
+        #Solution
 
 
-        #Initialization
-    solve.init_01(caseEl, solver)
-
-    solver.file.write(file_type = "case-data", file_name = caseEl["caseFilename"])
-    settingsFilename = "\"" + caseEl["caseFilename"] + ".set\""
-    solver.tui.file.write_settings(settingsFilename)
-
-        #Solve
-    if caseEl["solution"]["runSolver"]:
-        solve.solve_01(data, solver)
+           #Set Solver Settings
+        numerics.numerics_01(caseEl, solver)
+        #Activate Turbonumerics
 
 
-        filename = caseEl["caseFilename"] + "_fin"
-        solver.file.write(file_type = "case-data", file_name = filename)
+            #Initialization
+        solve.init_01(caseEl, solver)
 
-        filename = caseEl["caseFilename"] + "_" + caseEl["results"]["filename_outputParameter_pf"]
-    #solver.tui.define.parameters.output_parameters.write_all_to_file('filename')
-        tuicommand = "define parameters output-parameters write-all-to-file \"" + filename + "\""
-        solver.execute_tui(tuicommand)
-        filename = caseFilename + "_" + caseEl["results"]["filename_summary_pf"]
-        solver.results.report.summary(write_to_file = True, file_name = filename)
-        # Write out system time
-        solver.report.system.time_statistics()
+        solver.file.write(file_type = "case-data", file_name = caseEl["caseFilename"])
+        settingsFilename = "\"" + caseEl["caseFilename"] + ".set\""
+        solver.tui.file.write_settings(settingsFilename)
 
-      #Finalize
-    solver.file.stop_transcript()
-    #solver.exit()
+            #Solve
+        if caseEl["solution"]["runSolver"]:
+            solve.solve_01(caseEl, solver)
+
+            filename = caseEl["caseFilename"] + "_fin"
+            solver.file.write(file_type = "case-data", file_name = filename)
+
+            #postprocessing
+            filename = caseEl["caseFilename"] + "_" + caseEl["results"]["filename_outputParameter_pf"]
+        #solver.tui.define.parameters.output_parameters.write_all_to_file('filename')
+            tuicommand = "define parameters output-parameters write-all-to-file \"" + filename + "\""
+            solver.execute_tui(tuicommand)
+            filename = caseFilename + "_" + caseEl["results"]["filename_summary_pf"]
+            solver.results.report.summary(write_to_file = True, file_name = filename)
+            # Write out system time
+            solver.report.system.time_statistics()
+
+          #Finalize
+        solver.file.stop_transcript()
+
+# Do Studies
+studyDict = turboData.get("studies")
+if not (studyDict is None):
+    parametricstudy.study01(studyDict=studyDict, solver=solver)
+
+#Exit Solver
+solverExit = launchEl.get("exitatend", False)
+if solverExit:
+    solver.exit()
+
+print("Script successfully finished! \n")
---
+++ parametricstudy.py
@@ -0,0 +1,79 @@
+def study01(studyDict, solver, functionName=None):
+    #Init variables
+    fluent_study = None
+    studyIndex = 0
+
+    for studyName in studyDict:
+        studyEl = studyDict[studyName]
+        # Getting all input data from json file
+        datapath = studyEl.get("datapath")
+        refCase = studyEl.get("refCaseFilename")
+
+        # Read Ref Case
+        #solver.file.read_case_data(file_type="case-data", file_name=refCase)
+        solver.tui.file.read_case_data(refCase)
+
+        # Initialize a new parametric study
+        if fluent_study is None:
+            #fluent_study = ParametricStudy(solver.parametric_studies).initialize()
+            solver.parametric_studies.initialize(project_filename=studyName)
+            psname = refCase + "-Solve"
+            fluent_study = solver.parametric_studies[psname]
+
+        designPointCounter = 1
+        definitionList = studyEl.get("definition")
+
+        for studyDef in definitionList:
+            useScaleFactor = studyDef.get("useScaleFactor")
+            ipList = studyDef.get("inputparameters")
+            valueListArray = studyDef.get("valueList")
+            numDPs = len(valueListArray[0])
+            for dpIndex in range(numDPs):
+                #new_dp = fluent_study.add_design_point()
+                fluent_study.design_points.duplicate(design_point="Base DP")
+                designPointName = "DP" + str(designPointCounter)
+                #new_dp = {"BC_P_Out": 0.}
+                new_dp = fluent_study.design_points[designPointName]
+                numIPs = len(ipList)
+                for ipIndex in range(numIPs):
+                    ipName = ipList[ipIndex]
+                    modValue = valueListArray[ipIndex][dpIndex]
+                    if useScaleFactor:
+                        ref_dp = fluent_study.design_points["Base DP"].input_parameters()
+                        #ref_dp = {"ip1": 2.0, "BC_P_Out": 1.0, "ip3": 3.0}
+                        refValue = ref_dp[ipName]
+                        modValue = refValue * modValue
+
+                    # new_dp[ipName] = modValue
+                    new_dp.input_parameters = {ipName: modValue}
+                    new_dp.write_data = studyEl.get("write_data")
+
+                #fluent_study.design_points[designPointName].input_parameters = new_dp
+                designPointCounter = designPointCounter + 1
+
+        # Run all Design Points
+        if studyEl["updateAllDPs"]:
+            fluent_study.design_points.update_all()
+
+        # Export results to table
+        design_point_table = datapath + studyName + "_dp_table.csv"
+        #fluent_study.export_design_table(design_point_table)
+        solver.parametric_studies.export_design_table(filepath=design_point_table)
+
+        # Save Study
+        #solver.tui.file.parametric_project.save_as(studyName)
+        if studyIndex == 0:
+            solver.file.parametric_project.save()
+        else:
+            solver.file.parametric_project.save_as(project_filename=studyName)
+        #
+
+        if studyIndex < (len(studyDict) - 1):
+            # Delete DesignPoints Current Study
+            #fluent_study = fluent_study.duplicate()
+            for dpIndex in range(designPointCounter - 1):
+                designPointName = "DP" + str(dpIndex + 1)
+                fluent_study.design_points.delete_design_points(design_points=designPointName)
+
+        studyIndex = studyIndex + 1
+    print("All Studies finished")
