98f463cc4a7a199c1e1e35ce5055090ac77e9405
agantner <andreas.gantner@ansys.com>
2023-05-12 11:50:33 +0200

    - new version sets all discretization schemes to second order -> does not use fluent defaults anymore

--- numerics.py
+++ numerics.py
@@ -1,10 +1,17 @@
 def numerics_01(data, solver):
     #Fluent Defaults in v232
-    #solver.solution.methods.discretization_scheme["pressure"] = "second-order"
-    #solver.solution.methods.discretization_scheme["temperature"] = "second-order-upwind"
-    #solver.solution.methods.discretization_scheme["mom"] = "second-order-upwind"
-    #solver.solution.methods.discretization_scheme["k"] = "second-order-upwind"
-    #solver.solution.methods.discretization_scheme["omega"] = "second-order-upwind"
+    # solver.solution.methods.discretization_scheme["pressure"] = "second-order"
+    # solver.solution.methods.discretization_scheme["temperature"] = "second-order-upwind"
+    # solver.solution.methods.discretization_scheme["mom"] = "second-order-upwind"
+    # solver.solution.methods.discretization_scheme["k"] = "second-order-upwind"
+    # solver.solution.methods.discretization_scheme["omega"] = "second-order-upwind"
+
+    discDict = solver.solution.methods.discretization_scheme
+    for discKey in discDict:
+        if discKey == "pressure":
+            discDict[discKey] = "second-order"
+        else:
+            discDict[discKey] = "second-order-upwind"
 
     solver.solution.methods.gradient_scheme = "green-gauss-node-based"
     if data["solution"]["tsn"]:
