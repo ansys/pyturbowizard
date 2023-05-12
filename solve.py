b84b53c68f32b4c4e313dc22fc45638a88a9945b
agantner <andreas.gantner@ansys.com>
2023-05-12 13:06:03 +0200

    - solver.py: use standard & hybrid initialization as default

--- solve.py
+++ solve.py
@@ -1,7 +1,8 @@
 def init_01(data, solver):
     solver.tui.solve.initialize.compute_defaults.pressure_inlet(data["locations"]["bz_inlet_name"])
     solver.solution.initialization.standard_initialize()
-    solver.solution.initialization.fmg_initialize()
+    solver.solution.initialization.hybrid_initialize()
+    #solver.solution.initialization.fmg_initialize()
 
 def solve_01(data, solver):
     solver.solution.run_calculation.iterate(iter_count= data["solution"]["iter_count"])
