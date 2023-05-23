deb3a2d7120c51a4480bf3653cead57ad0ea2007
agantner <andreas.gantner@ansys.com>
2023-05-23 14:45:58 +0200

    - main.py: change order of initialization that initialization settings are also stored in cas file - mysetup.py: local scaling of residuals - mysetup.py: all conditions need to be met - config-file: set rescrit to fluent default

--- main.py
+++ main.py
@@ -132,15 +132,13 @@ if caseDict is not None:
                 data=caseEl, solver=solver, functionName=functionEl["numerics"]
             )
 
-        # Write case & settings file
-        solver.file.write(file_type="case", file_name=caseEl["caseFilename"])
-        settingsFilename = '"' + caseEl["caseFilename"] + '.set"'
-        solver.tui.file.write_settings(settingsFilename)
-
         # Initialization
         solve.init_01(caseEl, solver)
-        # Write initial data
-        solver.file.write(file_type="data", file_name=caseEl["caseFilename"])
+
+        # Write case and ini-data & settings file
+        solver.file.write(file_type="case-data", file_name=caseEl["caseFilename"])
+        settingsFilename = '"' + caseEl["caseFilename"] + '.set"'
+        solver.tui.file.write_settings(settingsFilename)
 
         # Solve
         if caseEl["solution"]["runSolver"]:
