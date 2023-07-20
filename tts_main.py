77974bf95204efb0e78443c5a00f16d03e0b743f
gantnera <andreas.gantner@ansys.com>
2023-07-20 11:18:54 +0200

    - tts_main.py: adding version to dict

--- tts_main.py
+++ tts_main.py
@@ -15,7 +15,7 @@ from tts_subroutines import (
 )
 
 
-version = "1.4.6"
+version = "1.4.7"
 print(f"\n*** Starting PyTurboVersion (Version {str(version)}) ***\n\n")
 
 # If solver variable does not exist, Fluent has been started in external mode
