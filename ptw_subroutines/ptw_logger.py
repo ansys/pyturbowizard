a78b2761f890a3d603f0cc5eec981b3ca216b2c2
gantnera <andreas.gantner@ansys.com>
2023-07-25 13:13:00 +0200

    - introducing ptw_logger.py

--- ptw_subroutines/ptw_logger.py
+++ ptw_subroutines/ptw_logger.py
@@ -1,4 +1,4 @@
-import logging, sys
+import logging
 
 logger = logging.getLogger('PyTurboWizard')
 
@@ -6,19 +6,19 @@ def init_logger(console_output:bool = True, file_output:bool = True):
     from ptw_subroutines import utilities
     logger.setLevel(logging.INFO)
     if console_output:
-        handler = logging.StreamHandler(sys.stdout)
+        handler = logging.StreamHandler()
         formatter = logging.Formatter(fmt='%(name)-12s: %(levelname)-8s - %(message)s')
         handler.setFormatter(formatter)
         handler.setLevel(logging.INFO)
         logger.addHandler(handler)
     if file_output:
-        loggerFileName = utilities.get_free_filename(dirname=".", base_filename='PyTurboWizard.log')
-        handler = logging.FileHandler(filename=loggerFileName, encoding='utf-8')
+        logger_file_name = utilities.get_free_filename(dirname=".", base_filename='PyTurboWizard.log')
+        handler = logging.FileHandler(filename=logger_file_name, encoding='utf-8')
         formatter = logging.Formatter(fmt='[%(asctime)s] {%(filename)s:%(lineno)d} %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
         handler.setFormatter(formatter)
         handler.setLevel(logging.DEBUG)
         logger.addHandler(handler)
-        logger.info(f"Logger-File-Handler: {loggerFileName}")
+        logger.info(f"Logger-File-Handler: {logger_file_name}")
     logger.info(f"Logger initialized")
     return logger
 
