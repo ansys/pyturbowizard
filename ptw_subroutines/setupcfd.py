6fc980a7f6b4b46c0d6afb4a0e5b79ddd1fd2494
gantnera <andreas.gantner@ansys.com>
2024-02-09 11:35:32 +0100

    - setupcfd.py: fixing type condition bug

--- ptw_subroutines/setupcfd.py
+++ ptw_subroutines/setupcfd.py
@@ -746,7 +746,8 @@ def set_boundaries(data, solver, solveEnergy: bool = True):
                     f"Creation of periodic interface is skipped!"
                 )
             else:
-                # As the origin & axis have been set for all cell-zones these are the defaults for all containing boundary zones
+                # As the origin & axis have been set for all cell-zones,
+                # these are the defaults for all containing boundary zones
                 # Therefore, we do not need to set them
                 try:
                     rotation_angle = peri_if_El[key_if].get("rotation_angle")
