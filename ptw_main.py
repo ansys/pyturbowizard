662be752d8e134137b9d991e8b9bd7ca018ff8ca
Andreas Gantner <128615962+gantnera@users.noreply.github.com>
2025-06-11 13:25:16 +0200

    Development branch (#199)
    - ptw_main.py: Updating to v1.9.6
    - setupcfd.py: bug fix for creating general interfaces
    - updating incompressible turbine template
    
    ---------
    
    Co-authored-by: benjamintraut <132369007+benjamintraut@users.noreply.github.com>
    Co-authored-by: benjamintraut <benjamin.traut@ansys.com>
    Co-authored-by: hauke2007 <108066434+hauke2007@users.noreply.github.com>
    Co-authored-by: ManuelHelferich <147180186+ManuelHelferich@users.noreply.github.com>
    Co-authored-by: ManuelHelferich <manuel.helferich@ansys.com>
    Co-authored-by: mkaimasi <magda.kaimasidou@ansys.com>
    Co-authored-by: nemti <nemti@users.noreply.github.com>

--- ptw_main.py
+++ ptw_main.py
@@ -28,7 +28,7 @@ from ptw_subroutines.utils import (
 )
 
 
-ptw_version = "1.9.5"
+ptw_version = "1.9.6"
 
 # Set Logger
 logger = ptw_logger.init_logger()
--- ptw_subroutines/setupcfd.py
+++ ptw_subroutines/setupcfd.py
@@ -872,7 +872,22 @@ def set_boundaries(data, solver, solve_energy: bool = True, gpu: bool = False):
                     zone_list=[side1, side2], new_type="interface"
                 )
                 # Create Interface
-                solver.settings.setup.mesh_interfaces.create_manually(name=key_if, zone_list_1=[side1],zone_list_2=[side2])
+                if Version(solver._version) < Version("251"):
+                    if not gpu:
+                        solver.tui.define.turbo_model.turbo_create(
+                            key_if, side1, "()", side2, "()", "3"
+                        )
+                    else:
+                        if list(keyEl.keys()).index(key_if) == 0:
+                            key_if = key_if.replace("-", "_")
+                            solver.tui.define.mesh_interfaces.create(
+                                key_if, "no", side1, side2, "()", "no"
+                            )
+                        else:
+                            key_if = key_if.replace("-", "_")
+                            solver.tui.define.mesh_interfaces.create(key_if, side1, side2)
+                else:
+                    solver.settings.setup.mesh_interfaces.interface.create(name=key_if,zone1_list=[side1],zone2_list=[side2])
 
     # Setup turbo-interfaces at end
     keyEl = data["locations"].get("bz_interfaces_mixingplane_names")
